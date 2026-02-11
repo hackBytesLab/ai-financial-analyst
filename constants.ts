1902a67a05b2801fa659800af3774642cc6b5813
PeteKunGz <pestkung2547@gmail.com>
2026-02-11 15:09:29 +0700

    test pete

--- constants.ts
+++ constants.ts
@@ -2,7 +2,7 @@
 import { Transaction } from './types';
 
 export const INITIAL_TRANSACTIONS: Transaction[] = [
-  { id: '1', type: 'income', amount: 12500, category: 'Salary', date: '2024-06-01', note: 'Monthly Salary' },
+  { id: '1', type: 'income', amount: 10, category: 'Salary', date: '2024-06-01', note: 'Monthly Salary' },
   { id: '2', type: 'expense', amount: 2870, category: 'Food', date: '2024-06-05', note: 'Groceries' },
   { id: '3', type: 'expense', amount: 2050, category: 'Rent', date: '2024-06-02', note: 'Apartment' },
   { id: '4', type: 'expense', amount: 1640, category: 'Transport', date: '2024-06-10', note: 'Fuel' },
